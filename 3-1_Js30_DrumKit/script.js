95e595e5f6733f7f398c6a61cdebafb7c5450df2
cedejeet <108671045+cedejeet@users.noreply.github.com>
2022-07-22 15:40:48 +0200

    incomplet

---
+++ 3-1_Js30_DrumKit/script.js
@@ -0,0 +1,18 @@
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const keySelect = document.querySelector(`div[data-key="${e.code}"]`);
+  if (!audio) return;
+
+  keySelect.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
